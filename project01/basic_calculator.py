efadf05d6d7efdadf4503a444a2e5a1f59a0ad0a
José Henrique da Silva <1josehenrique.jh@gmail.com>
2023-08-13 09:18:54 -0300

    Update: novo modo de cálculo - exponenciação + melhoria no código e uso da função clear_output da biblioteca ipython

--- project01/basic_calculator.py
+++ project01/basic_calculator.py
@@ -1,64 +1,100 @@
-print("Olá, Essa é uma Calculadora que irá te auxiliar em seus cálculos!")
+from IPython.display import clear_output
 
 while True:
-    operation = int(input("Escolha qual tipo de operação deseja fazer:\n(1) ADIÇÃO\n(2) SUBTRAÇÃO\n(3) MULTIPLICAÇÃO\n(4) DIVISÃO\n"))
 
-    if operation == 1:
-        print("--------------------------------")
-        print("Você escolheu ADIÇÃO!\nDigite um número:")
-        num1 = int(input())
-        print("---------------------------------")
-        print("Digite outro número:")
-        num2 = int(input())
-        calc = num1 + num2
-        print("---------------------------------")
-        print(f"RESULTADO: {calc}")
-        print("---------------------------------")
+  print("Digite: ")
+  print(" ")
+  print("1 para Soma")
+  print("2 para Subtração")
+  print("3 para Multiplicação")
+  print("4 para Divisão")
+  print("5 para Exponenciação")
+  print(" ")
 
-    if operation == 2 :
-        print("---------------------------------")
-        print("Você escolheu SUBTRAÇÃO!\nDigite um número:")
-        num1 = int(input())
-        print("---------------------------------")
-        print("Digite outro número:")
-        num2 = int(input())
-        calc = num1 - num2
-        print("---------------------------------")
-        print(f"RESULTADO: {calc}")
-        print("---------------------------------")
+  modo = int(input())
 
-    if operation == 3 :
-        print("---------------------------------")
-        print("Você escolheu MULTIPLICAÇÃO!\nDigite um número:")
-        num1 = int(input())
-        print("---------------------------------")
-        print("Digite outro número:")
-        num2 = int(input())
-        calc = num1 * num2
-        print("---------------------------------")
-        print(f"RESULTADO: {calc}")
-        print("---------------------------------")
+  if modo == 1:  
+    print("====================")
+    print("Você escolheu SOMA!")
+    print("====================")
+    print("Digite um número: ")
+    n1 = float(input())
 
-    if operation == 4 :
-        print("---------------------------------")
-        print("Você escolheu DIVISÃO!\nDigite um número:")
-        num1 = int(input())
-        print("---------------------------------")
-        print("Digite outro número:")
-        num2 = int(input())
-        calc = num1 / num2
-        print("---------------------------------")
-        print(f"RESULTADO: {calc}")
-        print("---------------------------------")
+    print("Digite outro número: ")
+    n2 = float(input())
 
-    if operation >= 5:
-        print("Modo de operação inválida!\nDigite um modo de operação válido!")
-        print("---------------------------------")
+    print("====================")
+    resultado = n1 + n2
 
-    if operation == 0:
-        print("Modo de operação inválida!\nDigite um modo de operação válido!".upper())
-        print("---------------------------------")
+    print("{} + {} = {}".format(n1, n2, resultado))
+    print("====================")
 
-    resposta = input("Deseja fazer outro cálculo? (s/n): ")
-    if resposta.lower() != "s":
-        break
+  elif modo == 2:  
+    print("====================")
+    print("Você escolheu SUBTRAÇÃO!")
+    print("====================")
+    print("Digite um número: ")
+    n1 = float(input())
+
+    print("Digite outro número: ")
+    n2 = float(input())
+
+    print("====================")
+    resultado = n1 - n2
+
+    print("{} - {} = {}".format(n1, n2, resultado))
+    print("====================")
+
+  elif modo == 3:  
+    print("====================")
+    print("Você escolheu MULTIPLICAÇÃO!")
+    print("====================")
+    print("Digite um número: ")
+    n1 = float(input())
+
+    print("Digite outro número: ")
+    n2 = float(input())
+
+    print("====================")
+    resultado = n1 * n2
+
+    print("{} x {} = {}".format(n1, n2, resultado))
+    print("====================")
+
+  elif modo == 4:  
+    print("====================")
+    print("Você escolheu DIVISÃO!")
+    print("====================")
+    print("Digite um número: ")
+    n1 = float(input())
+
+    print("Digite outro número: ")
+    n2 = float(input())
+
+    print("====================")
+    resultado = n1 / n2
+
+    print("{} / {} = {}".format(n1, n2, resultado))
+    print("====================")
+
+  elif modo == 5:  
+    print("====================")
+    print("Você escolheu EXPONENCIAÇÃO!")
+    print("====================")
+    print("Valor da base: ")
+    base = float(input())
+
+    print("Valor do expoente: ")
+    expoente = float(input())
+
+    print("====================")
+    resultado = pow(base, expoente)
+
+    print("{} ^ {} = {}".format(base, expoente, resultado))
+    print("====================")
+
+    clear_output(wait=True)
+
+  resposta = input("Deseja fazer outro cálculo? (s/n): ")
+  if resposta.lower() != 's':
+    break
