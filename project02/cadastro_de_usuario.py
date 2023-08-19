ca6102dd8dbf212bed1003165d13ed66f304d4d9
José Henrique da Silva <1josehenrique.jh@gmail.com>
2023-08-19 11:35:04 -0300

    Novo Projeto: Cadastro de funcionário, usando random e datetime

---
+++ project02/cadastro_de_usuario.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+import random
+
+def info_usuario():
+    nome = input('Olá, digite o seu nome:\n')
+    print("====================")
+    idade = int(input('Quantos anos você tem?\n'))
+    print("====================")
+    dia = (datetime.now().day)
+    mes = (datetime.now().month)
+    ano = (datetime.now().year)
+    cartoes = ['R$50,00','R$250,00','R$120,00']
+    cartao = (random.choice(cartoes))
+    aniversario = datetime.strptime(input('Qual a data do seu aniversário?\n'),'%d/%m/%Y')
+    print("====================")
+    print(f'Olá {nome}, seu registro foi concluido com sucesso no dia {dia}/{mes}/{ano}.\nParabéns, houve um sorteio e você ganhou um cartão de compras no valor de {cartao}.')
+   
+info_usuario()
